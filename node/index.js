a210a6d6803a15df2a41b0801a31b4296d78a4f0
zxcpadik <viktorandrukhin2000@gmail.com>
2023-11-07 21:16:40 +0300

    File rebase

--- node/index.js
+++
@@ -1,7 +0,0 @@
-const markActive = function(obj) {
-    let elements = document.getElementsByClassName('navbtn');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.remove('active');
-    }
-    obj.classList.add('active');
-}
